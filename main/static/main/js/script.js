bfa1023ef10b559c6867c7ee39f6a479632834e8
MrDrenix <to.sotomayor@duocuc.cl>
2024-10-20 19:43:25 -0300

    Detalles

--- main/static/main/js/script.js
+++ main/static/main/js/script.js
@@ -6,3 +6,11 @@ function toggleButtons() {
         buttonContainer.style.display = "none";
     }
 }
+
+function openModal() {
+    document.getElementById('profileOptions').style.display = 'flex';
+}
+
+function closeModal() {
+    document.getElementById('profileOptions').style.display = 'none';
+}
