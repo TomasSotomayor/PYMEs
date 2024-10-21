de006c13037bb497058e093a81b7b46ebb9f265a
MrDrenix <to.sotomayor@duocuc.cl>
2024-10-20 22:58:05 -0300

    Commit

--- main/static/main/js/script.js
+++ main/static/main/js/script.js
@@ -1,8 +1,25 @@
 function profileOptions() {
     var profileOptions = document.getElementById("profile-options");
+    var configOptions = document.getElementById("config-options");
+    if (configOptions.style.display === "block") {
+        configOptions.style.display = "none";
+    }
     if (profileOptions.style.display === "none" || profileOptions.style.display === "") {
         profileOptions.style.display = "block";
     } else {
         profileOptions.style.display = "none";
+    }
+}
+
+function configOptions() {
+    var configOptions = document.getElementById("config-options");
+    var profileOptions = document.getElementById("profile-options");
+    if (profileOptions.style.display === "block") {
+        profileOptions.style.display = "none";
+    }
+    if (configOptions.style.display === "none" || configOptions.style.display === "") {
+        configOptions.style.display = "block";
+    } else {
+        configOptions.style.display = "none";
     }
 }
