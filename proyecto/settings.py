c496bf0601eb1bb9b62d6d122119f2ef8fdc5c06
MrDrenix <to.sotomayor@duocuc.cl>
2024-11-19 22:49:25 -0300

    Avanzando

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'PYMEs/main/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'main', 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
